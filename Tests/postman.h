83209576bd42d60ed76681b45e7420ec6d84e847
david <davidespinhamarques@gmail.com>
2021-10-28 19:16:09 +0100

    first commit

---
+++ Tests/postman.h
@@ -0,0 +1,24 @@
+#ifndef SRC_POSTMAN_H_
+#define SRC_POSTMAN_H_
+
+#include "mail.h"
+#include <string>
+#include <vector>
+
+class Postman {
+public:
+	unsigned int id;
+	string name;
+	vector<Mail *> myMail;
+
+//public:
+	Postman();
+	void setName(string nm);
+	void addMail(Mail *m);
+	void addMail(vector<Mail *> mails);
+	string getName() const;
+	vector<Mail *> getMail() const;
+	unsigned int getID() const;
+};
+
+#endif /* SRC_POSTMAN_H_ */
